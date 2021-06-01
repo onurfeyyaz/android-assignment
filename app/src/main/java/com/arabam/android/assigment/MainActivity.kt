bbb0623da4eeaa9ff85264d9368df05ac5ce7f51
Feyyaz ONUR <feyyazonur0@gmail.com>
2021-06-01 16:45:19 +0300

    initial and nav&splash screen added

--- app/src/main/java/com/arabam/android/assigment/MainActivity.kt
+++ app/src/main/java/com/arabam/android/assigment/MainActivity.kt
@@ -1,19 +1,11 @@
-package com.arabam.android.assigment
+package com.feyyazonur.arabamcom
 
-import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
     }
-
 }
---
+++ app/src/main/java/com/arabam/android/assigment/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.feyyazonur.arabamcom
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+
+class SplashScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashScreen_to_listingFragment)
+        }, 1000)
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+}
